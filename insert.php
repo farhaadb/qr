7d6c31f1401f2923bcdb7f7b06b524ea59165d89
Farhaad <farhaad.bux@gmail.com>
2014-11-15 12:29:32 +0200

    Adding IP

--- insert.php
+++ insert.php
@@ -25,7 +25,7 @@
 	
 	if(isset($_POST['submit']) && $qCheck == false && $sCheck == false)
 		{
-			$con = mysql_connect("http://dutnoticeboard.co.za","root","athens");
+			$con = mysql_connect("107.170.89.145","root","athens");
 			
 			if (!$con)
 			{
